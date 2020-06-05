e50e56f1698e2690db375e732505f43d95978f5b
tell-y <t.yamamoto28@gmail.com>
2020-06-06 00:45:11 +0900

    modify to deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devtool : '#source-map',
   output: {
-    path: path.join(__dirname,"dist"),
+    path: path.join(__dirname,"public"),
     filename: '[name].js',
     publicPath : "http://localhost:8080/assets"
   },
